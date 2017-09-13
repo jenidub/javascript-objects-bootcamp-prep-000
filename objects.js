85824fa8f92d39ffe444e6656bca488482f6f1dd
Jeni Williams <stanfordjeni@gmail.com>
2017-09-13 16:47:15 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {Pharrell: "Happy"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle
+  playlist[artistName] = songTitle
   return playlist
 }
